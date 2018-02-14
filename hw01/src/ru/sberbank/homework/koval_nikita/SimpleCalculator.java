b1baadf1dfa77f35449a9e0dd9b32d71ddfb9d73
KovalNikita <st010774@student.spbu.ru>
2018-02-14 15:16:12 +0300

    Update SimpleCalculator.java
    Поменял названия методов (add -> Addition)

--- hw01/src/ru/sberbank/homework/koval_nikita/SimpleCalculator.java
+++ hw01/src/ru/sberbank/homework/koval_nikita/SimpleCalculator.java
@@ -1,28 +1,35 @@
 package ru.sberbank.homework.koval_nikita;
 
-class SimpleCalculator{
-	public static int add(int a, int b){
-		return a + b;
-	}
-	public static int sub(int a, int b){
-		return a - b;
-	}
-	public static int mult(int a, int b){
-		return a * b;
-	}
-	public static int div(int a, int b){
-		return a / b;
-	}
-	public static double add(double a, double b){
-		return a + b;
-	}
-	public static double sub(double a, double b){
-		return a - b;
-	}
-	public static double mult(double a, double b){
-		return a * b;
-	}
-	public static double div(double a, double b){
-		return a / b;
-	}
+class SimpleCalculator {
+    public static int Addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int Subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public static int Multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static int Division(int a, int b) {
+        return a / b;
+    }
+
+    public static double Addition(double a, double b) {
+        return a + b;
+    }
+
+    public static double Subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public static double Multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public static double Division(double a, double b) {
+        return a / b;
+    }
 }
