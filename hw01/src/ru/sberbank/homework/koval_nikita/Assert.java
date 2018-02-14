3f11d4e025a3bb110df4578924feb177043436f6
KovalNikita <st010774@student.spbu.ru>
2018-02-14 15:14:12 +0300

    Update Assert.java
    Поменял модификаторы константы и изменил тип исключений

--- hw01/src/ru/sberbank/homework/koval_nikita/Assert.java
+++ hw01/src/ru/sberbank/homework/koval_nikita/Assert.java
@@ -1,15 +1,17 @@
 package ru.sberbank.homework.koval_nikita;
 
 public class Assert {
-	static double eps = 1e-5;
-	public static void assertIntsIsEqual(String message, int  expected, int  actual) throws Exception{
-		if(expected != actual){
-			throw new Exception(message);
-		}
-	}
-	public static void assertDoublesIsEqual(String message, double  expected, double  actual) throws Exception{
-		if( Math.abs(expected - actual)  >= eps ){
-			throw new Exception(message);
-		}
-	}
+    private static final double DELTA = 1e-5;
+
+    public static void assertIntsAreEqual(String message, int expected, int actual) {
+        if (expected != actual) {
+            throw new RuntimeException(message);
+        }
+    }
+
+    public static void assertDoublesAreEqual(String message, double expected, double actual) {
+        if (Math.abs(expected - actual) >= DELTA) {
+            throw new RuntimeException(message);
+        }
+    }
 }
