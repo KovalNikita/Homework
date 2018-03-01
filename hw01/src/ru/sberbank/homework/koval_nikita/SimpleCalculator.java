ccef0223054f743d79cbe377531e7ef2acd4c88b
KovalNikita <koval.nikita.95@mail.ru>
2018-03-01 12:20:24 +0300

    Исправлены названия методов, добавлен тест деления на 0

--- hw01/src/ru/sberbank/homework/koval_nikita/SimpleCalculator.java
+++ hw01/src/ru/sberbank/homework/koval_nikita/SimpleCalculator.java
@@ -1,35 +1,35 @@
 package ru.sberbank.homework.koval_nikita;
 
 class SimpleCalculator {
-    public static int Addition(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 
-    public static int Subtraction(int a, int b) {
+    public static int subtract(int a, int b) {
         return a - b;
     }
 
-    public static int Multiplication(int a, int b) {
+    public static int multiply(int a, int b) {
         return a * b;
     }
 
-    public static int Division(int a, int b) {
+    public static int divide(int a, int b) {
         return a / b;
     }
 
-    public static double Addition(double a, double b) {
+    public static double add(double a, double b) {
         return a + b;
     }
 
-    public static double Subtraction(double a, double b) {
+    public static double subtract(double a, double b) {
         return a - b;
     }
 
-    public static double Multiplication(double a, double b) {
+    public static double multiply(double a, double b) {
         return a * b;
     }
 
-    public static double Division(double a, double b) {
+    public static double divide(double a, double b) {
         return a / b;
     }
 }
--- hw01/src/ru/sberbank/homework/koval_nikita/SimpleCalculatorTest.java
+++ hw01/src/ru/sberbank/homework/koval_nikita/SimpleCalculatorTest.java
@@ -1,69 +1,73 @@
 package ru.sberbank.homework.koval_nikita;
 
-import ru.sberbank.homework.koval_nikita.SimpleCalculator;
-import ru.sberbank.homework.koval_nikita.Assert;
-
 public class SimpleCalculatorTest {
     public static void main(String[] args) {
 
-        testIntAddition();
-        testIntSubtraction();
-        testIntMultiplication();
-        testIntDivision();
+        addIntTest();
+        subtractIntTest();
+        multiplyIntTest();
+        divideIntTest();
 
-        testDoubleAddition();
-        testDoubleSubtraction();
-        testDoubleMultiplication();
-        testDoubleDivision();
+        addDoubleTest();
+        subtractDoubleTest();
+        multiplyDoubleTest();
+        divideDoubleTest();
+        divideByZeroDoubleTest();
 
         System.out.println("All tests were passed successfully!");
     }
 
-    private static void testIntAddition() {
-        int actual = SimpleCalculator.Addition(2, 3);
+    private static void addIntTest() {
+        int actual = SimpleCalculator.add(2, 3);
         int expected = 5;
-        Assert.assertIntsAreEqual("Error in testIntAddition", expected, actual);
+        Assert.assertIntsAreEqual("Error in addIntTest", expected, actual);
     }
 
-    private static void testIntSubtraction() {
-        int actual = SimpleCalculator.Subtraction(21, 8);
+    private static void subtractIntTest() {
+        int actual = SimpleCalculator.subtract(21, 8);
         int expected = 13;
-        Assert.assertIntsAreEqual("Error in testIntSubtraction", expected, actual);
+        Assert.assertIntsAreEqual("Error in subtractIntTest", expected, actual);
     }
 
-    private static void testIntMultiplication() {
-        int actual = SimpleCalculator.Multiplication(19, 2);
+    private static void multiplyIntTest() {
+        int actual = SimpleCalculator.multiply(19, 2);
         int expected = 38;
-        Assert.assertIntsAreEqual("Error in testIntMultiplication", expected, actual);
+        Assert.assertIntsAreEqual("Error in multiplyIntTest", expected, actual);
     }
 
-    private static void testIntDivision() {
-        int actual = SimpleCalculator.Division(14, 6);
+    private static void divideIntTest() {
+        int actual = SimpleCalculator.divide(14, 6);
         int expected = 2;
-        Assert.assertIntsAreEqual("Error in testIntDivision", expected, actual);
+        Assert.assertIntsAreEqual("Error in divideIntTest", expected, actual);
     }
 
-    private static void testDoubleAddition() {
-        double actual = SimpleCalculator.Addition(8.4, 3.6);
+    private static void addDoubleTest() {
+        double actual = SimpleCalculator.add(8.4, 3.6);
         double expected = 12;
-        Assert.assertDoublesAreEqual("Error in testDoubleAddition", expected, actual);
+        Assert.assertDoublesAreEqual("Error in addDoubleTest", expected, actual);
     }
 
-    private static void testDoubleSubtraction() {
-        double actual = SimpleCalculator.Subtraction(8.31, 0.2);
+    private static void subtractDoubleTest() {
+        double actual = SimpleCalculator.subtract(8.31, 0.2);
         double expected = 8.11;
-        Assert.assertDoublesAreEqual("Error in testDoubleSubtraction", expected, actual);
+        Assert.assertDoublesAreEqual("Error in subtractDoubleTest", expected, actual);
     }
 
-    private static void testDoubleMultiplication() {
-        double actual = SimpleCalculator.Multiplication(2.47, 3.4);
+    private static void multiplyDoubleTest() {
+        double actual = SimpleCalculator.multiply(2.47, 3.4);
         double expected = 8.398;
-        Assert.assertDoublesAreEqual("Error in testDoubleMultiplication", expected, actual);
+        Assert.assertDoublesAreEqual("Error in multiplyDoubleTest", expected, actual);
     }
 
-    private static void testDoubleDivision() {
-        double actual = SimpleCalculator.Division(9.86, 4.8);
+    private static void divideDoubleTest() {
+        double actual = SimpleCalculator.divide(9.86, 4.8);
         double expected = 2.0541667;
-        Assert.assertDoublesAreEqual("Error in testDoubleDivision", expected, actual);
+        Assert.assertDoublesAreEqual("Error in divideDoubleTest", expected, actual);
+    }
+
+    private static void divideByZeroDoubleTest() {
+        double actual = SimpleCalculator.divide(9.86, 0.0);
+        double expected = Double.POSITIVE_INFINITY;
+        Assert.assertDoublesAreEqual("Error in divideByZeroDoubleTest", expected, actual);
     }
 }
